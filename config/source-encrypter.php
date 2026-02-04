893b9144631642bb119474a0b43a82b80bef694b
evkrnapr <fitriaputrimuwafiq@gmail.com>
2026-02-04 10:21:59 +0700

    qwe

--- config/source-encrypter.php
+++ config/source-encrypter.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'source'      => ['app', 'database','routes','config','resources'], // Path(s) to encrypt
+    'source'      => ['app', 'database'], // Path(s) to encrypt
     'destination' => 'encrypted', // Destination path
     'key_length'  => 16, // Encryption key length
 ];
